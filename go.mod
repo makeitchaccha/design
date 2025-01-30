b5cdd2ff5784771f9146d8759a3226e6e07c80b1
yuyaprgrm <yuyaprgrm@outlook.jp>
2025-01-30 10:37:03 +0900

    v0.0.1

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/makeitchaccha/design
+
+go 1.23.4
+
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/makeitchaccha/rendering v0.0.3
+	golang.org/x/image v0.23.0
+)
---
+++ timeline/timeline_builder.go
@@ -0,0 +1,28 @@
+package timeline
+
+import "time"
+
+type TimelineBuilder struct {
+	Timeline
+}
+
+func NewTimelineBuilder(start, end time.Time) *TimelineBuilder {
+	b := &TimelineBuilder{}
+	b.StartTime = start
+	b.EndTime = end
+	return b
+}
+
+func (b *TimelineBuilder) AddEntries(entries ...Entry) *TimelineBuilder {
+	b.Entries = append(b.Entries, entries...)
+	return b
+}
+
+func (b *TimelineBuilder) SetIndicator(indicator time.Time) *TimelineBuilder {
+	b.Indicator = indicator
+	return b
+}
+
+func (b *TimelineBuilder) Build() Timeline {
+	return b.Timeline
+}
